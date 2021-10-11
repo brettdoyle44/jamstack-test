022a51cafd5c1db246ea217839de92d5f8c9e1c8
brettdoyle44@gmail.com <h0ck3ypl@y3r44>
2021-10-10 22:11:25 -0400

    Try again

---
+++ functions/deleteLink.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const formattedResponse = require('./utils/formattedResponse')
+const {DELETE_LINK} = require('./utils/linkQueries')
+const sendQuery = require('./utils/sendQuery')
+
+exports.handler = async (event) => {
+    const { id } = JSON.parse(event.body)
+    const variables = { id }
+    try {
+        const { deleteLink: deletedLink } = await sendQuery(DELETE_LINK, variables)
+
+        return formattedResponse(200, deletedLink)
+    } catch(err) {
+        console.error(err)
+        return formattedResponse(500, { err: 'Something went wrong'})
+    }
+}
